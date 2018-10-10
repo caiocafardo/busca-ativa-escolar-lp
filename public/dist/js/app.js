ba1605d1ab47aea7ab7c508fce6f671d3a44963f
Sandy Santos <ssantos@unicef.org>
2018-10-10 16:56:32 -0300

    Ajuste url dinamica

--- public/dist/js/app.js
+++ public/dist/js/app.js
@@ -11452,80 +11452,90 @@ window.unidades_fededativas = [{
     }];
 
 // Scroll
-$(function() {
-	$('a[href*="#"]:not([href="#"])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: target.offset().top -90
-				}, 1000);
-				$('#nav').toggleClass("open");
-				return false;
-			}
-		}
-	});
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 90
+                }, 1000);
+                $('#nav').toggleClass("open");
+                return false;
+            }
+        }
+    });
 });
 
 //Menu
 $('.hamburger').click(function (ev) {
-	console.log('click');
-	ev.preventDefault();
-	$('#nav').toggleClass("open");
+    console.log('click');
+    ev.preventDefault();
+    $('#nav').toggleClass("open");
 });
 
 //Mask
-$(function() {
-	$('.mask-phone').mask('(99) 9999-9999?9');
-	$('.mask-phone-no-ddd').mask('9999-9999?9');
-	$('.mask-ddd').mask('(99)');
-	$('.mask-cep').mask('99999-999');
-	$('.mask-cpf').mask('999.999.999-99');
+$(function () {
+    $('.mask-phone').mask('(99) 9999-9999?9');
+    $('.mask-phone-no-ddd').mask('9999-9999?9');
+    $('.mask-ddd').mask('(99)');
+    $('.mask-cep').mask('99999-999');
+    $('.mask-cpf').mask('999.999.999-99');
 });
 
 //AJAX
-$('form').submit(function(e) {
-	e.preventDefault();
-	var button = $(".btn-submit");
-	button.prop("disabled", true);
-	button.removeClass("yellow");
-	$.ajax({
-		method: this.method,
-		url: this.action,
-		data: $(this).serialize()
-		}
-	).done(function(data) {
-		button.removeAttr("disabled");
-		button.addClass("yellow");
-		data.status
-			? swal("Tudo certo!", data.mensagem, "success")
-			: swal("Ops...", data.mensagem, "error")
-	});
+$('form').submit(function (e) {
+    e.preventDefault();
+    var button = $(".btn-submit");
+    button.prop("disabled", true);
+    button.removeClass("yellow");
+    $.ajax({
+            method: this.method,
+            url: this.action,
+            data: $(this).serialize()
+        }
+    ).done(function (data) {
+        button.removeAttr("disabled");
+        button.addClass("yellow");
+        data.status
+            ? swal("Tudo certo!", data.mensagem, "success")
+            : swal("Ops...", data.mensagem, "error")
+    });
 });
 
 //SELECT ESTADOS E MUNICÍPIOS
-$(document).ready(function(){
-	$estados = $('#estados');
-	$municipios = $('#cidades');
-	unidades_fededativas.forEach(function(uf){
-		var uf_option = document.createElement("option");
-		uf_option.value = uf.sigla;
-		uf_option.text = uf.sigla;
-		$estados.append(uf_option);
-	});
+$(document).ready(function () {
+    $estados = $('#estados');
+    $municipios = $('#cidades');
+    unidades_fededativas.forEach(function (uf) {
+        var uf_option = document.createElement("option");
+        uf_option.value = uf.sigla;
+        uf_option.text = uf.sigla;
+        $estados.append(uf_option);
+    });
+
+    if ('buscaativaescolar.test' === location.host) {
+        $("#acessar_plataforma").attr("href", "//panel.busca-ativa-escolar.test");
+    }
+    if ('site.testes.buscaativaescolar.org.br' === location.host) {
+        $("#acessar_plataforma").attr("href", "//plataforma.testes.buscaativaescolar.org.br");
+    }
+
 });
 
-$("#estados").on("change", function(){
-	$("#cidades").children().not(':first').remove();
-	var estado = $('#estados').val();
-    unidades_fededativas.forEach(function(uf){
-		if(estado != uf.sigla){return}
-        uf.cidades.forEach(function(municipio){
+$("#estados").on("change", function () {
+    $("#cidades").children().not(':first').remove();
+    var estado = $('#estados').val();
+    unidades_fededativas.forEach(function (uf) {
+        if (estado != uf.sigla) {
+            return
+        }
+        uf.cidades.forEach(function (municipio) {
             var municipio_option = document.createElement("option");
             municipio_option.value = municipio;
             municipio_option.text = municipio;
             $municipios.append(municipio_option);
         });
-	});
-})
+    });
+});
